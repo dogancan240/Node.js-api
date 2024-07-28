f69b73bc699980699848d4edaa97f02965e9d7b6
Dogancan Demir <dogancandemir2001@gmail.com>
2024-07-28 21:52:19 +0300

    Update ormconfig.js

--- app/config/ormconfig.js
+++ app/config/ormconfig.js
@@ -1,10 +1,10 @@
 module.exports = {
   type: "mysql",
-  host: process.env.DB_HOST || "sql7.freesqldatabase.com",
+  host: process.env.DB_HOST || ,
   port: process.env.DB_PORT || 3306,
-  username: process.env.DB_USERNAME || "sql7707551",
-  password: process.env.DB_PASSWORD || "iSnIEXWCr6",
-  database: process.env.DB_DATABASE || "sql7707551",
+  username: process.env.DB_USERNAME || "",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_DATABASE || "",
   synchronize: false, // if there is no table make it true and run once, then make it false
   logging: false,
   entities: ["dist/models/**/*.js"],
